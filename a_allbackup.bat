6a8a7da7b533aad6bf2f347b7a3b4bd869388505
Raul Herrera <raulch626@gmail.com>
2025-03-21 14:55:42 -0600

    Updated file uploaded

--- a_allbackup.bat
+++ a_allbackup.bat
@@ -11,19 +11,20 @@ echo Backup Script
 echo "Currently Backing Up Gitik2Share"
 
 :: Perform the backup using robocopy
-robocopy "G:\My Drive\Gitik2Share Drive" "F:\BACKUPS\Gmail Accounts\Gitik2Share" /MIR /XO /Z /ZB /R:5 /W:5 /TBD /NP /V /MT:16
+robocopy "G:\My Drive\Gitik2Share Drive" "F:\BACKUPS\Gmail Accounts\Gitik2Share" /E /XO /DCOPY:DAT /COPYALL /R:3 /W:10 /TEE /MT:16
+
 
 :: Display the current Google Drive to be backed up
 echo "Currently Backing Up raulch626"
 
 :: Perform the backup using robocopy
-robocopy "I:\My Drive" "F:\BACKUPS\Gmail Accounts\raulch626" /MIR /XO /Z /ZB /R:5 /W:5 /TBD /NP /V /MT:16
+robocopy "I:\My Drive" "F:\BACKUPS\Gmail Accounts\raulch626" /E /XO /DCOPY:DAT /COPYALL /R:3 /W:10 /TEE /MT:16
 
 :: Display the current Google Drive to be backed up
 echo "Currently Backing Up gitik2archives"
 
 :: Perform the backup using robocopy
-robocopy "J:\My Drive" "F:\BACKUPS\Gmail Accounts\gitik2archives" /MIR /XO /Z /ZB /R:5 /W:5 /TBD /NP /V /MT:16
+robocopy "J:\My Drive" "F:\BACKUPS\Gmail Accounts\gitik2archives" /E /XO /DCOPY:DAT /COPYALL /R:3 /W:10 /TEE /MT:16
 
 echo Backup process completed.
 
