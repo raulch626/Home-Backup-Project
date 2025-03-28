0e297f07c5cd2c338b120fa3cb3b1657270cb08f
Raul Herrera <raulch626@gmail.com>
2025-03-28 14:48:17 -0600

    Update allbackup.bat
    Added the PAUSE command so that it will pause every time a new backup is finished for each of the 3 of my Gmail accounts.

--- allbackup.bat
+++ allbackup.bat
@@ -9,21 +9,23 @@ echo Backup Script
 
 :: Display the current Google Drive to be backed up
 echo "Currently Backing Up Gitik2Share"
-
 :: Perform the backup using robocopy
-robocopy "G:\My Drive\Gitik2Share Drive" "F:\BACKUPS\Gmail Accounts\Gitik2Share" /MIR /XO /Z /ZB /R:5 /W:5 /TBD /NP /V /MT:16
+robocopy "G:\My Drive\Gitik2Share Drive" "F:\BACKUPS\Gmail Accounts\Gitik2Share" /E /XO /DCOPY:DAT /COPYALL /R:3 /W:10 /TEE /MT:16
+PAUSE
+
 
 :: Display the current Google Drive to be backed up
 echo "Currently Backing Up raulch626"
-
 :: Perform the backup using robocopy
-robocopy "I:\My Drive" "F:\BACKUPS\Gmail Accounts\raulch626" /MIR /XO /Z /ZB /R:5 /W:5 /TBD /NP /V /MT:16
+robocopy "I:\My Drive" "F:\BACKUPS\Gmail Accounts\raulch626" /E /XO /DCOPY:DAT /COPYALL /R:3 /W:10 /TEE /MT:16
+PAUSE
 
 :: Display the current Google Drive to be backed up
 echo "Currently Backing Up gitik2archives"
-
 :: Perform the backup using robocopy
-robocopy "J:\My Drive" "F:\BACKUPS\Gmail Accounts\gitik2archives" /MIR /XO /Z /ZB /R:5 /W:5 /TBD /NP /V /MT:16
+robocopy "J:\My Drive" "F:\BACKUPS\Gmail Accounts\gitik2archives" /E /XO /DCOPY:DAT /COPYALL /R:3 /W:10 /TEE /MT:16
+PAUSE
 
 echo Backup process completed.
+echo Thank you!
 
