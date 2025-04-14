255bf0194de033346a8f212774a8c71c9170876a
Raul Herrera <raulch626@gmail.com>
2025-04-14 17:04:33 -0600

    Added Gitik2AI6 to the backup

--- a_allbackup.bat
+++ a_allbackup.bat
@@ -11,20 +11,28 @@ echo Backup Script
 echo "Currently Backing Up Gitik2Share"
 
 :: Perform the backup using robocopy
-robocopy "G:\My Drive\Gitik2Share Drive" "F:\BACKUPS\Gmail Accounts\Gitik2Share" /E /XO /DCOPY:DAT /COPYALL /R:3 /W:10 /TEE /MT:16
+robocopy "G:\My Drive\Gitik2Share Drive" "D:\BACKUPS\Gmail Accounts\Gitik2Share" /E /XO /DCOPY:DAT /COPYALL /R:3 /W:10 /TEE /MT:16
+
+:: Display the current Google Drive to be backed up
+echo "Currently Backing Up gitik2AI6"
+
+:: Perform the backup using robocopy
+robocopy "H:\My Drive" "D:\BACKUPS\Gmail Accounts\gitik2AI6" /E /XO /DCOPY:DAT /COPYALL /R:3 /W:10 /TEE /MT:16
 
 
 :: Display the current Google Drive to be backed up
 echo "Currently Backing Up raulch626"
 
 :: Perform the backup using robocopy
-robocopy "I:\My Drive" "F:\BACKUPS\Gmail Accounts\raulch626" /E /XO /DCOPY:DAT /COPYALL /R:3 /W:10 /TEE /MT:16
+robocopy "I:\My Drive" "D:\BACKUPS\Gmail Accounts\raulch626" /E /XO /DCOPY:DAT /COPYALL /R:3 /W:10 /TEE /MT:16
 
 :: Display the current Google Drive to be backed up
 echo "Currently Backing Up gitik2archives"
 
 :: Perform the backup using robocopy
-robocopy "J:\My Drive" "F:\BACKUPS\Gmail Accounts\gitik2archives" /E /XO /DCOPY:DAT /COPYALL /R:3 /W:10 /TEE /MT:16
+robocopy "J:\My Drive" "D:\BACKUPS\Gmail Accounts\gitik2archives" /E /XO /DCOPY:DAT /COPYALL /R:3 /W:10 /TEE /MT:16
+
+
 
 echo Backup process completed.
 echo Thank you!
